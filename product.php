4c850fde19e9e334787773907fab2a65ec584985
finnb <finnbroekhoven@gmail.com>
2026-01-13 10:37:33 +0100

    Product aangemaakt

--- product.php
+++ product.php
@@ -1,3 +1,4 @@
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -5,11 +6,31 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Producten</title>
+    <link rel="stylesheet" href="css/product.css">
 </head>
 <body>
 
+
+<nav>
+
+</nav>
+
+
+<header>
+
+</header>
+
+
+<main>
+
+</main>
+
+
+<footer>
+
+</footer>
+
+
 </body>
 </html>
-
-Tester test test test test test
