435849977c6ce15b3f7138f52cf9def672eb8e93
finnb <finnbroekhoven@gmail.com>
2026-01-14 13:26:25 +0100

    Details done

--- detail.php
+++ detail.php
@@ -30,7 +30,26 @@ mysqli_close($db);
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Playball&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap');
+    </style>
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <nav>
+        <div class="logo">
+            <a href="index.php"><img src="images/Logo_JandeVisman.png" alt="Jan de Visman"></a>
+        </div>
+        <div class="links">
+            <a href="index.php">Home</a>
+            <a href="products.php">Producten</a>
+            <a href="gallery.php">Galerij</a>
+            <a href="contact.php">Contact</a>
+        </div>
+    </nav>
+
+
     <title>Details - <?= $fish['name'] ?></title>
 </head>
 <body>
@@ -47,6 +66,23 @@ mysqli_close($db);
         <a class="button" href="products.php">Terug naar productpagina</a>
     </div>
 </div>
+
+<footer>
+    <div class="footerLeft">
+        <div>
+            <a href="index.php"><img src="images/Logo_Footer_JandeVisman.png" alt="" class="footerLogo"></a>
+        </div>
+        <div>
+            <p>© 2022 Jan de Visman</p>
+        </div>
+        <div>
+            <a href="https://www.facebook.com/jandevisman/"><img src="images/facebooklogo.png" alt="" class="mediaLogo"></a>
+            <a href="https://www.instagram.com/jande_visman/"><img src="images/instalogo.png" alt="" class="mediaLogo"></a>
+        </div>
+    </div>
+</footer>
 </body>
+
+
 </html>
 
