8d5f7a404dfa9f8bf9f3ad8a3ebcdeaba3de2f20
Jayden <jaydenvlekke06@gmail.com>
2026-01-15 14:02:24 +0100

    Minor changes

--- fishbasket.php
+++ fishbasket.php
@@ -6,31 +6,21 @@ $fishName = $_SESSION['name'];
 $fishFull = $_SESSION['full_name'];
 $fishPrice = $_SESSION['price_range'];
 
+if (isset($_SESSION['name'])) {
+    $emptyOrder = false;
+}
+
+if (!isset($_SESSION['name'])) {
+    $emptyOrder = true;
+
+
+    require_once "includes/connection.php";
+}
+
 if (!isset($_SESSION['name']) || $_SESSION['name'] == '') {
   $emptyOrder = true;
     exit;
 }
-
-
-if (isset($_SESSION['name'])) {
-    $emptyOrder = false;
-
-
-    require_once "includes/connection.php";
-
-    $fishId = mysqli_escape_string($db, $_GET['id']);
-
-    $query = "SELECT * FROM fishes WHERE id = '$fishId'";
-    $result = mysqli_query($db, $query);
-
-    if (mysqli_num_rows($result) != 1) {
-        exit;
-    }
-}
-
-$fish = mysqli_fetch_assoc($result);
-
-mysqli_close($db);
 ?>
 
 
