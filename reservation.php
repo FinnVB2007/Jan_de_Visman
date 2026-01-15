135d7fc06e6cd1688ef1da42aeb434936aec0cd5
SkAnnika <korstenannika@gmail.com>
2026-01-15 12:23:21 +0100

    reserveringspagina heeft nu afbeelding (die nog aangepast moet worden eigenlijk)

--- reservation.php
+++ reservation.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Reserveren</title>
-    <link rel="stylesheet" href="css/product.css">
+    <link rel="stylesheet" href="css/reservation.css">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Playball&display=swap');
         @import url('https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap');
@@ -25,7 +25,26 @@
         <a href="contact.php">Contact</a>
     </div>
 </nav>
+<header>
+    <h1>Reserveer hier</h1>
+</header>
+<main>
 
 
+</main>
+<footer>
+    <div class="footerLeft">
+        <div>
+            <a href="index.php"><img src="images/Logo_Footer_JandeVisman.png" alt="" class="footerLogo"></a>
+        </div>
+        <div>
+            <p>© 2022 Jan de Visman</p>
+        </div>
+        <div>
+            <a href="https://www.facebook.com/jandevisman/"><img src="images/facebooklogo.png" alt="" class="mediaLogo"></a>
+            <a href="https://www.instagram.com/jande_visman/"><img src="images/instalogo.png" alt="" class="mediaLogo"></a>
+        </div>
+    </div>
+</footer>
 </body>
 </html>
