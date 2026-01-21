64818196e5f752ee7a4133d28984dd77d7e73eb6
finnb <finnbroekhoven@gmail.com>
2026-01-21 10:11:56 +0100

    fix img

--- products.php
+++ products.php
@@ -68,6 +68,7 @@ mysqli_close($db);
     <article>
         <h2><?= $fish['name'] ?></h2>
         <p><?= $fish['price_range'] ?></p>
+        <img src="image.php?id=<?php echo $fish['id']; ?>" width="250">
         <div class="buttons">
             <form action="detail.php">
         <a href="detail.php?id=<?= $fish['id'] ?>">Details</a>
