cf3dd06d81d48e105dab921389743546a2d1e612
finnb <finnbroekhoven@gmail.com>
2026-01-23 11:06:11 +0100

    deel burbur

--- burger.php
+++ burger.php
@@ -73,8 +73,19 @@ if (isset($_POST['submit'])) {
             <h1>Over ons</h1>
             <p>
                 Een gymleraar en een communicatieadviseur die samen verse vis bereiden en verkopen uit een omgebouwde
-                paardentrailer...
-            </p>
+                paardentrailer. Is dat geen bijzonder verhaal? Waar ons verhaal begint? Bij een gezamenlijke passie voor
+                vis… en gezelligheid :-).
+                <br>
+                <br>
+                Jan deed zijn kennis en ervaring met vis op tijdens zijn bijbaan bij Visgilde Leen de Jong. Hij is
+                daarnaast op het gebied van klussen een alleskunner. Onze mooie viskar is daar het resultaat van. Gert
+                heeft vanuit zijn achtergrond veel kennis van communicatie en marketing. Zijn grote netwerk komt goed
+                van pas.
+                <br>
+                <br>
+
+                Ons doel? Leuke dingen doen, mooie plaatsen bezoeken, toffe mensen ontmoeten en hen blij maken met onze
+                verse vis!
         </div>
     </section>
 
@@ -82,7 +93,18 @@ if (isset($_POST['submit'])) {
         <div>
             <h1>Ons aanbod</h1>
             <p>
-                Met ons enthousiasme, verse visproducten en nostalgische viskar wordt jouw feest een succes! ...
+                Met ons enthousiasme, verse visproducten en nostalgische viskar wordt jouw feest een succes!
+                We hebben ervaring met kleine feestjes én grote evenementen… en alles wat daartussen zit.
+                <br>
+                <br>
+                Als het met vis te maken heeft, kunnen we het waarschijnlijk leveren. We hebben een breed assortiment.
+                Onze kibbeling valt enorm goed in de smaak. We zijn daarnaast gespecialiseerd in het roken van zalm. Met
+                onze mobiele oven roken we ook op locatie. Dat geeft niet alleen een heerlijke geur, vaak komen mensen
+                gezellig bij onze rookoven staan.
+                <br>
+                <br>
+                Onze viskar op jouw feestje? Vul bij ‘contact’ op onze website het formulier in en we nemen contact met
+                je op.
             </p>
         </div>
         <div><img src="images/Viskar.png" alt="Een paardenkar omgebouwd tot viskar"></div>
